71b82483ef46393fcfe302b8f6608c4c8934d9bf
HenryXie1 <xiehonghuicn@gmail.com>
2019-08-03 20:22:06 +1000

    init seclist none commit

---
+++ oke-webhook.go
@@ -0,0 +1,221 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	//"strings"
+
+	"github.com/golang/glog"
+	"k8s.io/api/admission/v1beta1"
+	admissionregistrationv1beta1 "k8s.io/api/admissionregistration/v1beta1"
+	//appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/kubernetes/pkg/apis/core/v1"
+)
+
+var (
+	runtimeScheme = runtime.NewScheme()
+	codecs        = serializer.NewCodecFactory(runtimeScheme)
+	deserializer  = codecs.UniversalDeserializer()
+
+	defaulter = runtime.ObjectDefaulter(runtimeScheme)
+)
+
+var (
+	ignoredNamespaces = []string{
+		//need to check all namespaces
+		//metav1.NamespaceSystem,
+		//metav1.NamespacePublic,
+	}
+	requiredannotations = map[string]string{
+		"service.beta.kubernetes.io/oci-load-balancer-security-list-management-mode": "None",
+		
+	}
+	
+)
+
+const (
+	admissionWebhookAnnotationValidateKey = "oke-admission-webhook-pe/validate"
+	admissionWebhookAnnotationMutateKey   = "oke-admission-webhook-pe/mutate"
+	admissionWebhookAnnotationStatusKey   = "oke-admission-webhook-pe/status"
+
+	NA = "not_available"
+)
+
+type WebhookServer struct {
+	server *http.Server
+}
+
+// Webhook Server parameters
+type WhSvrParameters struct {
+	port           int    // webhook server port
+	certFile       string // path to the x509 certificate for https
+	keyFile        string // path to the x509 private key matching `CertFile`
+	sidecarCfgFile string // path to sidecar injector configuration file
+}
+
+type patchOperation struct {
+	Op    string      `json:"op"`
+	Path  string      `json:"path"`
+	Value interface{} `json:"value,omitempty"`
+}
+
+func init() {
+	_ = corev1.AddToScheme(runtimeScheme)
+	_ = admissionregistrationv1beta1.AddToScheme(runtimeScheme)
+	// defaulting with webhooks:
+	// https://github.com/kubernetes/kubernetes/issues/57982
+	_ = v1.AddToScheme(runtimeScheme)
+}
+
+func admissionRequired(ignoredList []string, admissionAnnotationKey string, metadata *metav1.ObjectMeta) bool {
+	//always required to check when creating service
+	return true
+}
+
+func mutationRequired(ignoredList []string, metadata *metav1.ObjectMeta) bool {
+	//This is POC , we don't do mutation as of now
+	return false
+}
+
+func validationRequired(ignoredList []string, metadata *metav1.ObjectMeta) bool {
+	required := admissionRequired(ignoredList, admissionWebhookAnnotationValidateKey, metadata)
+	glog.Infof("Validation policy for %v/%v: required:%v", metadata.Namespace, metadata.Name, required)
+	return required
+}
+
+
+// validate services must have annotation : service.beta.kubernetes.io/oci-load-balancer-security-list-management-mode: None
+func (whsvr *WebhookServer) validate(ar *v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
+	req := ar.Request
+	var (
+		availableannotations            map[string]string
+		objectMeta                      *metav1.ObjectMeta
+		resourceNamespace, resourceName string
+	)
+
+	glog.Infof("AdmissionReview for Kind=%v, Namespace=%v Name=%v (%v) UID=%v patchOperation=%v UserInfo=%v",
+		req.Kind, req.Namespace, req.Name, resourceName, req.UID, req.Operation, req.UserInfo)
+
+	switch req.Kind.Kind {
+	case "Service":
+		var service corev1.Service
+		if err := json.Unmarshal(req.Object.Raw, &service); err != nil {
+			glog.Errorf("Could not unmarshal raw object: %v", err)
+			return &v1beta1.AdmissionResponse{
+				Result: &metav1.Status{
+					Message: err.Error(),
+				},
+			}
+		}
+		resourceName, resourceNamespace, objectMeta = service.Name, service.Namespace, &service.ObjectMeta
+		availableannotations = service.Annotations
+	}
+
+	if !validationRequired(ignoredNamespaces, objectMeta) {
+		glog.Infof("Skipping validation for %s/%s due to policy check", resourceNamespace, resourceName)
+		return &v1beta1.AdmissionResponse{
+			Allowed: true,
+		}
+	}
+
+	allowed := true
+	var result *metav1.Status
+	glog.Info("available annotations:", availableannotations)
+	glog.Info("required annotations", requiredannotations)
+	if len(availableannotations) == 0 {
+		allowed = false
+		result = &metav1.Status{
+		Reason: "required annotations value are not set",
+			}
+	} else {
+	for key, value := range availableannotations {
+		if requiredannotations[key] != value {
+			allowed = false
+			glog.Info("required annotations: " + key + ":" + requiredannotations[key] + " value are not set")
+			result = &metav1.Status{
+				Reason: "required annotations: value are not set",
+			}
+			break
+		}
+	}
+  }
+
+	return &v1beta1.AdmissionResponse{
+		Allowed: allowed,
+		Result:  result,
+	}
+}
+
+// main mutation process
+func (whsvr *WebhookServer) mutate(ar *v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
+	//This is a POC mutate can be added later
+	return &v1beta1.AdmissionResponse{
+		Allowed: false,
+	}
+}
+
+// Serve method for webhook server
+func (whsvr *WebhookServer) serve(w http.ResponseWriter, r *http.Request) {
+	var body []byte
+	if r.Body != nil {
+		if data, err := ioutil.ReadAll(r.Body); err == nil {
+			body = data
+		}
+	}
+	if len(body) == 0 {
+		glog.Error("empty body")
+		http.Error(w, "empty body", http.StatusBadRequest)
+		return
+	}
+
+	// verify the content type is accurate
+	contentType := r.Header.Get("Content-Type")
+	if contentType != "application/json" {
+		glog.Errorf("Content-Type=%s, expect application/json", contentType)
+		http.Error(w, "invalid Content-Type, expect `application/json`", http.StatusUnsupportedMediaType)
+		return
+	}
+
+	var admissionResponse *v1beta1.AdmissionResponse
+	ar := v1beta1.AdmissionReview{}
+	if _, _, err := deserializer.Decode(body, nil, &ar); err != nil {
+		glog.Errorf("Can't decode body: %v", err)
+		admissionResponse = &v1beta1.AdmissionResponse{
+			Result: &metav1.Status{
+				Message: err.Error(),
+			},
+		}
+	} else {
+		fmt.Println(r.URL.Path)
+		if r.URL.Path == "/mutate" {
+			admissionResponse = whsvr.mutate(&ar)
+		} else if r.URL.Path == "/validate" {
+			admissionResponse = whsvr.validate(&ar)
+		}
+	}
+
+	admissionReview := v1beta1.AdmissionReview{}
+	if admissionResponse != nil {
+		admissionReview.Response = admissionResponse
+		if ar.Request != nil {
+			admissionReview.Response.UID = ar.Request.UID
+		}
+	}
+
+	resp, err := json.Marshal(admissionReview)
+	if err != nil {
+		glog.Errorf("Can't encode response: %v", err)
+		http.Error(w, fmt.Sprintf("could not encode response: %v", err), http.StatusInternalServerError)
+	}
+	glog.Infof("Ready to write reponse ...")
+	if _, err := w.Write(resp); err != nil {
+		glog.Errorf("Can't write response: %v", err)
+		http.Error(w, fmt.Sprintf("could not write response: %v", err), http.StatusInternalServerError)
+	}
+}
